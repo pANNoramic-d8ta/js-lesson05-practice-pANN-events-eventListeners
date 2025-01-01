81d35a14f7b2e7ba060fe3ca2feaa8b739df4801
pANNoramic-d8ta <pannoramicd8ta@duck.com>
2024-12-31 21:09:31 -0500

    Added light / dark toggle details.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
-var fans =document.querySelector(".fans");
-var fish =document.querySelector(".fish");
-var pets =document.querySelector(".pets");
+var fans = document.querySelector(".fans");
+var fish = document.querySelector(".fish");
+var pets = document.querySelector(".pets");
+var themeSwitchInput = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
 
 fans.addEventListener("mouseover", function () {
-fans.innerText = "123K";
-fans.style.color = "#ff0066";
+    fans.innerText = "123K";
+    fans.style.color = "#ff0066";
 });
 
 fish.addEventListener("mouseover", function () {
-fish.innerText = "47K";
-fish.style.color = "#66ffff";
+    fish.innerText = "47K";
+    fish.style.color = "#66ffff";
 });
 
 pets.addEventListener("mouseover", function () {
-pets.innerText = "20K";
-pets.style.color = "#99ff99";
+    pets.innerText = "20K";
+    pets.style.color = "#99ff99";
+});
+
+
+
+themeSwitchInput.addEventListener("click", function () {
+    if (body.classList.contains("light")) {
+        body.classList.remove("light")
+        ball.classList.remove("move-right")
+        h1.innerText = "Stealth Quincy";
+    } else {
+        body.classList.add("light");
+        ball.classList.add("move-right");
+        h1.innerText = "Party Quincy";
+    }
+
 });
